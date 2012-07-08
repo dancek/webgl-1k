b710d6ac52af13fbc23ed5fd7528ec9a18d4bf14
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2012-07-09 02:05:22 +0300

    initial, working but large glsl runner

---
+++ 1.js
@@ -0,0 +1,106 @@
+var initTime = new Date().valueOf();
+// SHADERS
+// vertex shader is just a pass-through
+var p = [],
+    v = "attribute vec2 p;void main(){gl_Position=vec4(p,0,1);}",
+    f = [
+// shader 1
+'precision highp float;'+
+'uniform float ut;'+
+'uniform vec2 ur;'+
+
+'void main(void) {\
+    vec2 position = - 1.0 + 2.0 * gl_FragCoord.xy / ur.xy;\
+    float red = abs( sin( position.x * position.y + ut / 5.0 ) );\
+    float green = abs( sin( position.x * position.y + ut / 4.0 ) );\
+    float blue = abs( sin( position.x * position.y + ut / 3.0 ) );\
+    gl_FragColor = vec4( red, green, blue, 1.0 );\
+}',
+    ],
+    gl, mCanvas, mWidth, mHeight;
+
+    var VERTEX_SHADER = 0x8B31,
+        FRAGMENT_SHADER = 0x8B30,
+        ARRAY_BUFFER = 0x8892,
+        STATIC_DRAW = 0x88E4,
+        COLOR_BUFFER_BIT = 0x4000,
+        TRIANGLE_STRIP = 5,
+        FLOAT = 0x1406;
+
+    var main = function()
+    {
+            var w=800, h=600;
+            mCanvas.width = w;
+            mCanvas.height = h;
+            gl.viewport(0, 0, w, h);
+            
+            // Timeline
+            var t = (new Date().valueOf() - initTime)/1000;
+            var prg = p[0]; // NOTE: This is the first part
+            //for (var i = 0; t > parts[i][0]; i++) prg = p[parts[i][1]];
+            gl.useProgram(prg);
+
+            // Set time and screen size for the animation
+            gl.uniform1f(gl.getUniformLocation(prg, "ut"), t);
+            gl.uniform2f(gl.getUniformLocation(prg, "ur"), w, h);
+
+            // Draw
+            gl.drawArrays(TRIANGLE_STRIP, 0, 4);
+    };
+
+    // App startup
+    (function ()
+    {
+//        try
+//        {
+            // Init WebGL context
+            mCanvas = document.createElement("canvas");
+            document.body.appendChild(mCanvas);
+            var s = mCanvas.style;
+            s.position = "fixed";
+            s.left = s.top = 0;
+            gl = mCanvas.getContext('experimental-webgl');
+//            if (!gl)
+//            {
+//                alert("This demo requires WebGL");
+//                return;
+//            }
+
+            // Init shaders
+            for (var i = 0, j; i < 8; ++i)
+            {
+                p[i] = gl.createProgram();
+
+                j = gl.createShader(VERTEX_SHADER);
+                gl.shaderSource(j, v);
+                gl.compileShader(j);
+//                if (!gl.getShaderParameter(j, gl.COMPILE_STATUS)) alert("Vertex shader  "+ i + ": "+ gl.getShaderInfoLog(j));
+                gl.attachShader(p[i], j);
+
+                j = gl.createShader(FRAGMENT_SHADER);
+                gl.shaderSource(j, f[i]);
+                gl.compileShader(j);
+//                if (!gl.getShaderParameter(j, gl.COMPILE_STATUS)) alert("Fragment shader  "+ i + ": "+ gl.getShaderInfoLog(j));
+                gl.attachShader(p[i], j);
+
+                gl.linkProgram(p[i]);
+//                if (!gl.getProgramParameter(p[i], gl.LINK_STATUS)) alert("Link program  "+ i + ": "+ gl.getProgramInfoLog(p[i]));
+            }
+
+            // Init vertex buffer for a regular screen-sized quad
+            var vertices = [-1,-1, -1,1, 1,-1, 1,1];
+            gl.bindBuffer(ARRAY_BUFFER, gl.createBuffer());
+            gl.bufferData(ARRAY_BUFFER, new Float32Array(vertices), STATIC_DRAW);
+
+            // Set up position attribute
+            gl.enableVertexAttribArray(0);
+            gl.vertexAttribPointer(0, 2, FLOAT, false, 0, 0); // position
+
+            // Start the main loop
+            setInterval(main, 1);
+//        }
+//        catch (err)
+//        {
+//            alert("Error initializing: " + err.message);
+//        }
+    })();
